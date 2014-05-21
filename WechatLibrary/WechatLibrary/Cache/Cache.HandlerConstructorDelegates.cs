a020911a3737d9377440fc080ae103ecbbdde8ad
h82258652 <842053625@qq.com>
2014-05-21 13:38:55 +0800

    update
    update cache。

--- WechatLibrary/WechatLibrary/Cache/Cache.HandlerConstructorDelegates.cs
+++ WechatLibrary/WechatLibrary/Cache/Cache.HandlerConstructorDelegates.cs
@@ -25,57 +25,69 @@ namespace WechatLibrary.Cache
 
         /// <summary>
         /// 缓存语音消息处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> VoiceHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存视频消息处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> VideoHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存地理位置消息处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> LocationHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存链接消息处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> LinkHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存关注事件处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> SubscribeHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存扫描二维码关注事件处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> QRSubscribeHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存取消关注事件处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> UnsubscribeHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存扫描带参数二维码事件处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> QRScanHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存上传地理位置事件处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
         public static volatile ConcurrentDictionary<string, Delegate> UploadLocationHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
 
         /// <summary>
         /// 缓存自定义菜单点击事件处理类的构造函数委托。
+        /// Key1：开发者微信 Id。
+        /// Key2：自定义菜单按钮的 Key 值。
         /// </summary>
-        public static volatile ConcurrentDictionary<string, Delegate> MenuButtonClickHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
+        public static volatile ConcurrentDictionary<string, ConcurrentDictionary<string, Delegate>> MenuButtonClickHandlerConstructorDelegates = new ConcurrentDictionary<string, ConcurrentDictionary<string, Delegate>>();
 
         /// <summary>
         /// 缓存自定义菜单跳转事件处理类的构造函数委托。
+        /// Key：开发者微信 Id。
         /// </summary>
-        public static volatile ConcurrentDictionary<string, Delegate> MenuButtonViewHandlerConstructorDelegates = new ConcurrentDictionary<string, Delegate>();
+        public static volatile ConcurrentDictionary<string,  Delegate> MenuButtonViewHandlerConstructorDelegates = new ConcurrentDictionary<string,Delegate>();
     }
 }
---
+++ WechatLibrary/WechatLibrary/Cache/Cache.HandlerProcessRequestMethods.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WechatLibrary.Cache
+{
+    public partial class Cache
+    {
+        /// <summary>
+        /// 缓存文本消息处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> TextHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存图片消息处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> ImageHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存语音消息处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> VoiceHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存视频消息处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> VideoHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存地理位置消息处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> LocationHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存链接消息处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> LinkHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存关注事件处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> SubscribeHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存扫描二维码关注事件处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> QRSubscribeHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存取消关注事件处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> UnsubscribeHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存扫描带参数二维码事件处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> QRScanHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存上传地理位置事件处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> UploadLocationHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// 缓存自定义菜单点击事件处理类的 ProcessRequest 方法。
+        /// Key1：开发者微信 Id。
+        /// Key2：自定义菜单按钮的 Key 值。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, ConcurrentDictionary<string, MethodInfo>> MenuButtonClickHandlerProcessRequestMethods = new ConcurrentDictionary<string, ConcurrentDictionary<string, MethodInfo>>();
+
+        /// <summary>
+        /// 缓存自定义菜单跳转事件处理类的 ProcessRequest 方法。
+        /// Key：开发者微信 Id。
+        /// </summary>
+        public static volatile ConcurrentDictionary<string, MethodInfo> MenuButtonViewHandlerProcessRequestMethods = new ConcurrentDictionary<string, MethodInfo>();
+    }
+}
