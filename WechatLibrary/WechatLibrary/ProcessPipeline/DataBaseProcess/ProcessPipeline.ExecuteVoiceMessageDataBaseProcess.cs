f678f81056d7b3cbdd21e7e02e9ffa69d54aabc8
h82258652 <842053625@qq.com>
2014-05-22 14:07:56 +0800

    update
    add the missing file.

---
+++ WechatLibrary/WechatLibrary/ProcessPipeline/DataBaseProcess/ProcessPipeline.ExecuteVoiceMessageDataBaseProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WechatLibrary.ProcessPipeline
+{
+    public partial class ProcessPipeline
+    {
+        /// <summary>
+        /// 执行数据库语音消息的自动回复。
+        /// </summary>
+        /// <returns>是否执行成功。</returns>
+        public bool ExecuteVoiceMessageDataBaseProcess()
+        {
+            return false;
+        }
+    }
+}
