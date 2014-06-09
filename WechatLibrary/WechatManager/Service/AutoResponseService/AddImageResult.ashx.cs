a75eff1014c4d45a48f72cb1f1b6b9d2c5520673
h82258652 <842053625@qq.com>
2014-06-09 15:28:06 +0800

    update

---
+++ WechatLibrary/WechatManager/Service/AutoResponseService/AddImageResult.ashx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using Common.Serialization.Json;
+using WechatLibrary.Model;
+
+namespace WechatManager.Service.AutoResponseService
+{
+    /// <summary>
+    /// AddImageResult 的摘要说明
+    /// </summary>
+    public class AddImageResult : IHttpHandler, IRequiresSessionState
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var wechatId = context.Session["WechatId"] as string;
+            if (string.IsNullOrEmpty(wechatId) == true)
+            {
+                var responseObj = new
+                {
+                    success = false,
+                    info = "please login again!"
+                };
+                var json = JsonHelper.SerializeToJson(responseObj);
+                context.Response.ContentType = "text/json";
+                context.Response.Write(json);
+                return;
+            }
+
+            var mediaId = context.Request["mediaId"];
+            if (string.IsNullOrEmpty(mediaId) == true)
+            {
+                var responseObj = new
+                {
+                    success = false,
+                    info = "please input media id!"
+                };
+                var json = JsonHelper.SerializeToJson(responseObj);
+                context.Response.ContentType = "text/json";
+                context.Response.Write(json);
+                return;
+            }
+
+            using (var entities = new WechatEntities())
+            {
+                var query = entities.WechatAccounts.Where(temp => temp.WechatId == wechatId);
+                if (query.Count() <= 0)
+                {
+                    var responseObj = new
+                    {
+                        success = false,
+                        info = "please login again!"
+                    };
+                    var json = JsonHelper.SerializeToJson(responseObj);
+                    context.Response.ContentType = "text/json";
+                    context.Response.Write(json);
+                    return;
+                }
+                if (query.Count() > 1)
+                {
+                    var responseObj = new
+                    {
+                        success = false,
+                        info = "data base occurred an error!"
+                    };
+                    var json = JsonHelper.SerializeToJson(responseObj);
+                    context.Response.ContentType = "text/json";
+                    context.Response.Write(json);
+                    return;
+                }
+
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
