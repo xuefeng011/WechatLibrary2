090766f2b522a091b20c34826534d7f9941f8145
h82258652 <842053625@qq.com>
2014-06-09 17:09:41 +0800

    update

---
+++ WechatLibrary/WechatManager/Service/AutoResponseService/ModifyTextResult.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="ModifyTextResult.ashx.cs" Class="WechatManager.Service.AutoResponseService.ModifyTextResult" %>
---
+++ WechatLibrary/WechatManager/Service/AutoResponseService/ModifyTextResult.ashx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using Common.Serialization.Json;
+using WechatLibrary.Model;
+
+namespace WechatManager.Service.AutoResponseService
+{
+    /// <summary>
+    /// ModifyTextResult 的摘要说明
+    /// </summary>
+    public class ModifyTextResult : IHttpHandler,IRequiresSessionState
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var wechatId = context.Session["WechatId"] as string;
+            if (string.IsNullOrEmpty(wechatId)==true)
+            {
+                var responseObj = new {success=false,info="please login again!"};
+                var json = JsonHelper.SerializeToJson(responseObj);
+                context.Response.ContentType = "text/json";
+                context.Response.Write(json);
+                return;
+            }
+
+            using (var entities=new WechatEntities())
+            {
+                
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
