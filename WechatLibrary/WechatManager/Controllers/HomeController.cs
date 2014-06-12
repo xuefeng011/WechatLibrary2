cd557ff3fe31674c6cb4ae4af6abf75558f712d0
h82258652 <842053625@qq.com>
2014-06-12 16:22:00 +0800

    update

--- WechatLibrary/WechatManager/Controllers/HomeController.cs
+++ WechatLibrary/WechatManager/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WechatLibrary.Interface.Handler;
+using WechatLibrary.Model.Message.Response;
+using WechatLibrary.Service;
+using EmptyResult = System.Web.Mvc.EmptyResult;
 
 namespace WechatManager.Controllers
 {
@@ -20,5 +24,41 @@ namespace WechatManager.Controllers
         {
             return View();
         }
+
+#if DEBUG
+        public ActionResult Test()
+        {
+            WechatLibrary.Wechat.ProcessRequest();
+            return new EmptyResult();
+        }
+#endif
+    }
+
+}
+
+#if DEBUG
+namespace HZFTest
+{
+    public class TextHandler : ITextHandler
+    {
+        public WechatLibrary.Model.Message.Response.ResponseResultBase ProcessRequest(WechatLibrary.Model.Message.Request.Normal.TextMessage message, ref bool dbProcess)
+        {
+            var pre = MessageLogService.GetPrevTextMessage(message);
+            if (pre != null)
+            {
+                return new TextResult()
+                {
+                    Content = "prev is " + pre.Content
+                };
+            }
+            else
+            {
+                return new TextResult()
+                {
+                    Content = "prev is empty"
+                };
+            }
+        }
     }
 }
+#endif
